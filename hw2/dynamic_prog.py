2389279987a8fbfa519bbe1f56662a56266bc920
Ryan Phillips <wordsandcolor@gmail.com>
2014-02-19 14:46:10 -0800

    rough draft of code done... feel free to make any changes you want

--- hw2/dynamic_prog.py
+++ hw2/dynamic_prog.py
@@ -5,4 +5,24 @@ solves the max subarray problem via dynamic programming
 '''
 
 def dynamic_prog0(x):
-	return 0
+
+    this_sub_arr_sum = 0 
+    max_sum = 0
+    
+    for i in x:
+
+    	# print this_sub_arr_sum
+    			
+    	# does adding the last element make it bigger?
+    	if this_sub_arr_sum + i > 0: 
+    		this_sub_arr_sum = this_sub_arr_sum + i
+    	else:
+    		# failed to make a larger array so we are starting over 
+    		# at the new idx
+    		this_sub_arr_sum = 0
+
+    	if this_sub_arr_sum > max_sum:
+    		max_sum = this_sub_arr_sum
+
+    return max_sum
+			
---
+++ hw2/main.py
@@ -0,0 +1,76 @@
+import timeit, random, itertools, pylab
+import numpy as np
+from brute_force import *
+from div_and_conq import *
+from dynamic_prog import *
+
+# test cases
+t1 = [31,-41,59,26,-53,58,97,-93,-23,84] # 187
+t2 = [random.randint(-100,100) for r in xrange(10)]
+
+
+min_test = 100
+max_test = 800
+test_iter = 100
+
+
+#[algo][array_size][trial #]
+# setting up our test result array
+test_results = [[[1 for x in xrange(10)] for x in range(min_test,max_test+min_test,test_iter)] for x in xrange(3)]
+
+def test():
+	for idx, x in enumerate(range(min_test,max_test+min_test,test_iter)):
+		for y in xrange(10):
+			print 'testing',x,'trial',y
+			t0 = [random.randint(-100,100) for r in xrange(x)]
+			test_results[0][idx][y] = timeit.Timer(lambda: brute_force2(t0)).timeit(1)
+			#test_results[1][idx][y] = timeit.Timer(lambda: div_and_conq0(t0)).timeit(1)
+			test_results[2][idx][y] = timeit.Timer(lambda: dynamic_prog0(t0)).timeit(1)
+
+def show_graphs():
+
+	# n values (input array sizes)
+	_n = [[x for i in xrange(10)] for x in range(min_test,max_test+min_test,test_iter)]
+	_n = list(itertools.chain(*_n))
+
+	# flatten 2d array
+	_results1 = list(itertools.chain(*test_results[0]))
+	_results2 = list(itertools.chain(*test_results[1]))
+	_results3 = list(itertools.chain(*test_results[2]))
+
+	# plot raw data
+	pylab.loglog(_n,_results1,'ro',basex=10,basey=10, label="Brute Force")
+	#pylab.loglog(_n,_results2,'bo',basex=10,basey=10, label="Naive Divide & Conquer")
+	pylab.loglog(_n,_results3,'go',basex=10,basey=10, label="Dynamic Programming")
+	
+	# add labels and legend
+	pylab.xlabel('n')
+	pylab.ylabel('Time (s)')
+	
+
+	# plot best fit lines for all 3 data sets
+	slope,intercept=np.polyfit(np.log(_n),np.log(_results1),1)
+	t1 = np.arange(_n[0], _n[len(_n)-1], 0.01)
+	s1 = (2.71828**intercept)*t1**slope
+	pylab.loglog(t1, s1,'r',basex=10,basey=10,label='')
+
+	slope2,intercept2=np.polyfit(np.log(_n),np.log(_results2),1)
+	t2 = np.arange(_n[0], _n[len(_n)-1], 0.01)
+	s2 = (2.71828**intercept2)*t2**slope2
+	pylab.loglog(t2, s2,'b',basex=10,basey=10,label='')
+
+	slope3,intercept3=np.polyfit(np.log(_n),np.log(_results3),1)
+	t3 = np.arange(_n[0], _n[len(_n)-1], 0.01)
+	s3 = (2.71828**intercept3)*t3**slope3
+	pylab.loglog(t3, s3,'g',basex=10,basey=10,label='')
+
+	pylab.legend(loc='upper left')
+
+	pylab.axis('tight')
+
+	pylab.show()
+
+
+test()
+show_graphs()
+
