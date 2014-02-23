8806c3796777f2a48da4745111793541e77bad51
Ryan Phillips <wordsandcolor@gmail.com>
2014-02-22 18:38:28 -0800

    added pseudo code and graphs to writeup

--- hw2/main.py
+++ hw2/main.py
@@ -3,17 +3,18 @@ import numpy as np
 from brute_force import *
 from div_and_conq import *
 from dynamic_prog import *
+import cPickle as pickle
 
 # test cases
 t1 = [31,-41,59,26,-53,58,97,-93,-23,84] # 187
 t2 = [random.randint(-100,100) for r in xrange(10)]
 
 
-min_test = 10
-max_test = 5010
-test_iter = 500
+min_test = 1000
+max_test = 10000
+test_iter = 1000
 
-num_trials = 1
+num_trials = 10
 
 #[algo][array_size][trial #]
 # setting up our test result array
@@ -28,56 +29,6 @@ def test():
 			test_results[1][idx][y] = timeit.Timer(lambda: div_and_conq0(t0)).timeit(1)
 			test_results[2][idx][y] = timeit.Timer(lambda: dynamic_prog0(t0)).timeit(1)
 
-def show_graphs():
-
-	# n values (input array sizes)
-	_n = [[x for i in xrange(num_trials)] for x in range(min_test,max_test+min_test,test_iter)]
-	_n = list(itertools.chain(*_n))
-
-	# flatten 2d array
-	_results1 = list(itertools.chain(*test_results[0]))
-	_results2 = list(itertools.chain(*test_results[1]))
-	_results3 = list(itertools.chain(*test_results[2]))
-
-	# plot raw data
-	pylab.loglog(_n,_results1,'ro',basex=10,basey=10, label="Brute Force")
-	pylab.loglog(_n,_results2,'bo',basex=10,basey=10, label="Divide & Conquer")
-	pylab.loglog(_n,_results3,'go',basex=10,basey=10, label="Dynamic Programming")
-	
-	# add labels and legend
-	pylab.xlabel('n')
-	pylab.ylabel('Time (s)')
-	
-
-	# plot best fit lines for all 3 data sets
-	slope,intercept=np.polyfit(np.log(_n),np.log(_results1),1)
-	t1 = np.arange(_n[0], _n[len(_n)-1], 0.01)
-	s1 = (2.71828**intercept)*t1**slope
-	pylab.loglog(t1, s1,'r',basex=10,basey=10,label='')
-
-	slope2,intercept2=np.polyfit(np.log(_n),np.log(_results2),1)
-	t2 = np.arange(_n[0], _n[len(_n)-1], 0.01)
-	s2 = (2.71828**intercept2)*t2**slope2
-	pylab.loglog(t2, s2,'b',basex=10,basey=10,label='')
-
-	slope3,intercept3=np.polyfit(np.log(_n),np.log(_results3),1)
-	t3 = np.arange(_n[0], _n[len(_n)-1], 0.01)
-	s3 = (2.71828**intercept3)*t3**slope3
-	pylab.loglog(t3, s3,'g',basex=10,basey=10,label='')
-
-	print slope, slope2, slope3
-
-	pylab.legend(loc='upper left')
-
-	#pylab.axis('tight')
-
-	pylab.show()
-
-def main():
-	test()
-	show_graphs()
-
-#main()
 
 def test_correctness(_file):
 	expected_values = []
@@ -106,4 +57,91 @@ def test_correctness(_file):
 
 	print "passed all tests!"
 
-test_correctness('verify_2.txt')
+
+def show_graphs(test_results):
+
+	# n values (input array sizes)
+	_n = [[x for i in xrange(num_trials)] for x in range(min_test,max_test+min_test,test_iter)]
+	_n = list(itertools.chain(*_n))
+
+	# flatten 2d array
+	_results1 = list(itertools.chain(*test_results[0]))
+	_results2 = list(itertools.chain(*test_results[1]))
+	_results3 = list(itertools.chain(*test_results[2]))
+
+	# plot raw data
+	pylab.loglog(_n,_results1,'ro',basex=10,basey=10, label="Brute Force")
+	pylab.loglog(_n,_results2,'bo',basex=10,basey=10, label="Divide & Conquer")
+	pylab.loglog(_n,_results3,'go',basex=10,basey=10, label="Dynamic Programming")
+	
+	# add labels and legend
+	pylab.xlabel('n')
+	pylab.ylabel('Time (s)')
+	
+	# plot best fit lines for all 3 data sets
+	slope,intercept=np.polyfit(np.log(_n),np.log(_results1),1)
+	t1 = np.arange(_n[0], _n[len(_n)-1], 0.01)
+	s1 = (2.71828**intercept)*t1**slope
+	pylab.loglog(t1, s1,'r',basex=10,basey=10,label='')
+
+	slope2,intercept2=np.polyfit(np.log(_n),np.log(_results2),1)
+	t2 = np.arange(_n[0], _n[len(_n)-1], 0.01)
+	s2 = (2.71828**intercept2)*t2**slope2
+	pylab.loglog(t2, s2,'b',basex=10,basey=10,label='')
+
+	slope3,intercept3=np.polyfit(np.log(_n),np.log(_results3),1)
+	t3 = np.arange(_n[0], _n[len(_n)-1], 0.01)
+	s3 = (2.71828**intercept3)*t3**slope3
+	pylab.loglog(t3, s3,'g',basex=10,basey=10,label='')
+
+	print 'y-intercepts:', intercept, intercept2, intercept3
+	print 'slopes:', slope, slope2, slope3
+
+	pylab.legend(loc='upper left')
+
+	#pylab.axis('tight')
+
+	pylab.show()
+
+def show_graphs2(test_results):
+
+	# n values (input array sizes)
+	_n = [[x for i in xrange(num_trials)] for x in range(min_test,max_test+min_test,test_iter)]
+	_n = list(itertools.chain(*_n))
+
+	# flatten 2d array
+	_results1 = list(itertools.chain(*test_results[0]))
+	_results2 = list(itertools.chain(*test_results[1]))
+	_results3 = list(itertools.chain(*test_results[2]))
+
+
+	pylab.plot(_n,_results1,'ro', label="Brute Force")
+	pylab.plot(_n,_results2,'go', label="Divide & Conquer")
+	pylab.plot(_n,_results3,'bo', label="Dynamic Programming")
+
+	pylab.xlabel('n')
+	pylab.ylabel('Time (s)')
+	pylab.legend(loc='upper left')
+
+	pylab.show()
+
+
+
+def main():
+
+	test_correctness('verify_2.txt')
+
+	#test()
+	#save
+	#pickle.dump( test_results, open( "results.p", "wb" ) )
+
+	#load
+	test_results = pickle.load( open( "results.p", "rb" ) )
+
+	show_graphs(test_results)
+	show_graphs2(test_results)
+
+main()
+
+
+
