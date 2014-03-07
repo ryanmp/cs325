3b41de487ddc95c3e047c2c458d18952378ecd17
1n5aN1aC <joshuavillwo@gmail.com>
2014-03-07 15:58:04 -0800

    Generate random list once. Implements #3
    This makes it so we only have to generate the list once, with max size,
    then just use python list cutting to only get the first n elements from
    it each time.

--- hw3/main.py
+++ hw3/main.py
@@ -7,6 +7,24 @@ from algo_exact import *
 from algo_fastdumb import *
 from algo_greedy import *
 
+n0 = 2     #n0 - n1 the range of input sizes you want to calculate,
+n1 = 240   #And then try.
+seed = "0" #Seeds RNG
+set = []   #Don't touch.  used to init random list
+
+def generate_test_set(_n,_range):
+	global set
+	random.seed(seed)
+	set = []
+	for i in xrange(_n):
+		set.append((random.randrange(1,_range),random.randrange(1,_range)))
+	return set
+
+def return_set(max):
+	global set
+	#not done
+	return set[:max]
+
 def parse_input(file_name):
 	f = open(file_name)
 	lines_raw = f.readlines()
@@ -27,18 +45,11 @@ def route_length(cities,route):
 		distance += math.hypot(cities[idx2][0] - cities[idx1][0], cities[idx2][1] - cities[idx1][1])
 	return distance
 
-def generate_test_set(_seed,_n,_range):
-	random.seed(_seed)
-	ret = []
-	for i in xrange(_n):
-		ret.append((random.randrange(1,_range),random.randrange(1,_range)))
-	return ret
-
 # { algorithm to test, range for n (n0=smallest -> n1=largest) }
 def time_algo(f, n0, n1):
 	ret = []
 	for i in xrange(n0,n1):
-		t1 = generate_test_set("0",i,100)
+		t1 = return_set(i)
 		ret.append(timeit.Timer(lambda: f(t1)).timeit(1))
 	return ret, [i for i in range(n0,n1)], f
 
@@ -46,7 +57,7 @@ def time_algo(f, n0, n1):
 def batch_algo_lengths(f, n0, n1):
 	ret = []
 	for i in xrange(n0,n1):
-		t1 = generate_test_set("0",i,100)
+		t1 = return_set(i)
 		ret.append( route_length( t1, f(t1) ) )
 
 	return ret, [i for i in range(n0,n1)], f
@@ -58,11 +69,13 @@ def estimate_runtime(input_size, slope, intercept):
 	out = (2.71828**intercept)*slope**input_size
 	print str(datetime.timedelta(seconds=out))
 
+#initialize random inputs:
+generate_test_set(n1,100)
+
 #cities1 = parse_input("in/example-input-1.txt")
-cities1 = generate_test_set("0",9,200)
+cities1 = return_set(9)
 
 '''
-
 route = algo_greedy(cities1)
 tsp_grapher.plot_route(cities1,route)
 print route_length(cities1, route), route
@@ -74,11 +87,7 @@ print route_length(cities1, route), route
 route = algo_fastdumb(cities1)
 tsp_grapher.plot_route(cities1,route)
 print route_length(cities1, route), route
-
 '''
-
-n0 = 2
-n1 = 240
 
 #l1, r1, f1 = batch_algo_lengths(algo_exact,n0,n1) #doesn't like a list of just one city
 l2, r2, f2 = batch_algo_lengths(algo_greedy,n0,n1)
@@ -89,12 +98,4 @@ tsp_grapher.plot_lengths([l2,l3], [r2,r3], [f2,f3])
 t2, r2, f2 =  time_algo(algo_greedy,n0,n1)
 t3, r3, f3 =  time_algo(algo_fastdumb,n0,n1)
 slopes, intercepts = tsp_grapher.plot_timing([t2,t3],[r2,r3],[f2,f3])
-#estimate_runtime(11,slopes[0],intercepts[0]) 
-
-
-
-
-
-
-
-
+#estimate_runtime(11,slopes[0],intercepts[0])
