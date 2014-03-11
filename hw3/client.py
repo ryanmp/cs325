f74bae800965b1cadf1d236dfd383192cd3af755
1n5aN1aC <joshuavillwo@gmail.com>
2014-03-10 20:17:49 -0700

    Update Client for our packet types

--- hw3/client.py
+++ hw3/client.py
@@ -1,4 +1,6 @@
 import asyncore, socket, json, signal, threading
+from time import sleep
+from sys import stdout, exit
 
 #change these values only
 DEBUG = True
@@ -60,20 +62,36 @@ class AsyncClient(asyncore.dispatcher):
 	#Delegates them out to be processed
 	def handle_read(self):
 		jdata = self.recv(8192)
-		#print jdata
+		if DEBUG:
+			print jdata
 		#assuming we actually received SOMETHING.....
 		if jdata:
 			#lets load up that json!  (DOES NOT DEAL WITH INVALID JSON!)
 			data = json.loads(jdata)
-			if data['id'] == 0:
-				lol = data['payload']
-				dealKeepAlive(self, lol)
-			elif data['id'] == 2:
-				beginning = data['payload']
-				self.t.dealRangeAggignment(beginning)
-			elif data['id'] == 9:
-				reason = data['payload']
-				signal_handler(reason, 2)
+			if data['id'] == S_SEND_UPD:
+				print "We got our meta-info update"
+				payload = data['payload']
+				#dealMetaInfo(self, payload)
+			elif data['id'] == S_SERV_KIL:
+				print "Our server is shutting down! :("
+				sleep(10)
+				exit()		#quit
+			elif data['id'] == S_WORK_GRE:
+				payload = data['payload']
+				dealGreedyWork(self, payload)
+				#Server sent us some greedy algo work
+			elif data['id'] == S_WORK_MST:
+				payload = data['payload']
+				dealMSTWork(self, payload)
+				#Server sent us some MST algo work
+			elif data['id'] == S_IMP_SGMT:
+				payload = data['payload']
+				dealImproveSegment(self, payload)
+				#Server sent us some segment swapping improvement work
+			elif data['id'] == S_IMP_SCTY:
+				payload = data['payload']
+				dealImproveCity(self, payload)
+				#Server sent us some city swapping improvement work
 			else:
 				print 'something went wrong.'
 
@@ -114,10 +132,10 @@ class SenderThread(threading.Thread):
 		self.run()
 
 #Initialize by asking for remote host info
-HOST = raw_input("What ip would you like to connect to? ")
+HOST = raw_input("Server IP? (Defaults to localhost): ")
 if (HOST == ''):
 	HOST = '127.0.0.1'
-PORT = raw_input("Port? Default 31337) ")	#Default 31337
+PORT = raw_input("Server Port? (Defaults to 31337): ")	#Default 31337
 if (PORT == ''):
 	PORT = 31337
 
