9ca9ab2cd4d824c8d47bd1919d22c7c89f19e771
1n5aN1aC <joshuavillwo@gmail.com>
2014-03-11 16:34:05 -0700

    Revert "Remove Old Server Code"
    This reverts commit a3505d45acb21f14911b8968b40ad89e1d212217.

--- hw3/server.py
+++ hw3/server.py
@@ -10,6 +10,8 @@ DEBUG = True		# 3= show all packet data.
 #change these values only
 
 #Global Variables
+connectionClassList = []
+connectionSocketList = []
 shortest = sys.maxint
 cities = []
 route = []
@@ -33,6 +35,7 @@ S_IMP_SCTY = 31 #S -> C #Send improvement work, swapping cities
 
 #deal with signals
 def signal_handler(signum, frame):
+	server.sendKill();
 	if signum == 2:
 		signum = 'Control-c'
 	print 'SHUTDOWN!  Reason:', signum
@@ -145,6 +148,16 @@ class AsyncServer(asyncore.dispatcher):
 	def handle_request(self, channel, method, path, header):
 		print "blah"
 
+	#We have been told to shutdown!
+	#Make sure we send the shutdown packet first!
+	def sendKill(self):
+		for _socketobject in connectionSocketList:
+			try:
+				_socketobject.send( createPickle(self, S_SERV_KIL, 'Server says SHUTDOWN!') )
+				print str( _socketobject.getpeername()[0] ) + ':' + str( _socketobject.getpeername()[1] ) + ' was sent the shutdown signal!'
+			except Exception:
+				pass
+
 	#We got a client connection!
 	def handle_accept(self):
 		pair = self.accept()
@@ -154,6 +167,8 @@ class AsyncServer(asyncore.dispatcher):
 		else:
 			sock, addr = pair
 			handler = PacketHandler(sock, addr)
+			connectionClassList.append(self)
+			connectionSocketList.append(sock)
 
 	def handle_close(self):
 		print self.addr, 'has disconnecteed!'
