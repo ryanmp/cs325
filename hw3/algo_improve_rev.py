d4d1a8a2e1580b976ea74a479937a21d6075965b
Ryan Phillips <wordsandcolor@gmail.com>
2014-03-13 16:06:33 -0700

    added is_valid() to helpers and algo_combos()
    see main() for usage

--- hw3/algo_improve_rev.py
+++ hw3/algo_improve_rev.py
@@ -1,5 +1,6 @@
 from helpers import *
 
+
 def algo_improve_rev(cities,route,seg_length):
 
 	_length = len(cities)-seg_length
@@ -21,3 +22,27 @@ def algo_improve_rev(cities,route,seg_length):
 			route = new_route
 
 	return route
+
+
+def algo_improve_rev_old(cities,route,seg_length):
+
+	for i in xrange(len(cities)-seg_length):
+
+		new_route = route[0:]
+
+		initial_length = route_length(cities,new_route)
+
+		start_idx = i
+		end_idx = i+seg_length
+
+		tmp = new_route[start_idx:end_idx]
+		tmp.reverse()
+		new_route[start_idx:end_idx] = tmp
+
+		new_length = route_length(cities,new_route)
+
+		if new_length < initial_length:
+			route = new_route
+
+	return route
+
