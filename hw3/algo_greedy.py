7ba4ee93eba917209393b0c3afd6e2a307470b29
1n5aN1aC <joshuavillwo@gmail.com>
2014-03-14 15:30:50 -0700

    Experimental

--- hw3/algo_greedy.py
+++ hw3/algo_greedy.py
@@ -5,24 +5,25 @@ from a starting city, it continually adds the next closest city to the route
 '''
 
 def algo_greedy(cities):
-    route = [cities[0]]
-    unvisited = cities[1:]
-    while unvisited:
-    	closest_dist = sys.maxint
-    	closest_idx = 0
-    	for i in unvisited:
-    		distance = math.hypot(i[0] - route[-1][0], i[1] - route[-1][1])
-    		if (distance < closest_dist):
-    			closest_dist = distance
-    			closest_idx = i
-    	route.append(closest_idx)
-        unvisited.remove(closest_idx)
+	route = [cities[0]]
+	unvisited = cities[1:]
+	while unvisited:
+		closest_dist = sys.maxint
+		closest_idx = 0
+		for i in unvisited:
+			#distance = math.hypot(i[0] - route[-1][0], i[1] - route[-1][1])
+			distance = add(math.pow(i[0] - route[-1][0], 2), math.pow((i[1] - route[-1][1]), 2))
+			if (distance < closest_dist):
+				closest_dist = distance
+				closest_idx = i
+		route.append(closest_idx)
+		unvisited.remove(closest_idx)
 
-    route2 = []
-    for i in route:
-    	route2.append(cities.index(i))
+	route2 = []
+	for i in route:
+		route2.append(cities.index(i))
 
-    return route2
+	return route2
 
 #Same as algo_greedy, but accepts a parameter to choose what city to start at.
 #This is for use when you want to calculate greedy from multiple starting positions
